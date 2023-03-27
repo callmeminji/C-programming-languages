ea2963755c1a2b51d9268e2a5adf9127cc648430
callmeminji <giyomkoala@gmail.com>
2023-03-27 13:54:15 +0900

    initial_

--- MyFirstProj/MyFirstProj.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>	//preprocessor instruction
-
-
-int main()
-{	// start of scope
-	printf("Love Ya!");	//call or invoke(함수호출) * not only pfintf
-	return 0;	//value return	
-}	//end of scope
